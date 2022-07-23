af4040edc2cba16aff2905a746748151dbecfeb5
Jonathan Reyes <79552541+J0nRey@users.noreply.github.com>
2022-07-23 17:18:12 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+//  const colors = require('colors')
+//  console.log('Hola Jonathan! bienvenido'.rainbow);
+
+const colors = require('colors/safe')
+console.log(colors.rainbow('Hola Jonathan! bienvenido'));
+
+// saludo don nuestro nombre en colores arcoiris
+// Hola Jonathan! bienvenido
+
+// Documentacion: https://www.npmjs.com/package/colors
